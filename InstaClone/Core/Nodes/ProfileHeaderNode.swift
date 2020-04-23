98955e57e0c628ec35fcaa9c5ebda92994174bce
Phil Martin <phil.martin1983@gmail.com>
2020-04-23 23:23:55 +0100

    UI Tidy up

--- InstaClone/Core/Nodes/ProfileHeaderNode.swift
+++ InstaClone/Core/Nodes/ProfileHeaderNode.swift
@@ -24,15 +24,18 @@ class ProfileHeaderAndStatsNode: BaseNode {
         let rightPadding = ASInsetLayoutSpec( insets: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 50),
                                               child: stack)
         rightPadding.style.flexGrow = 1
-        return ASStackLayoutSpec(direction: .horizontal, spacing: 40, justifyContent: .start, alignItems: .center, children: [profileImageSpec, rightPadding])
+        let ratioSpec = ASRatioLayoutSpec(ratio: 0.5, child: rightPadding)
+        return ASStackLayoutSpec(direction: .horizontal, spacing: 30, justifyContent: .start, alignItems: .center, children: [profileImageSpec, ratioSpec])
     }
     
     var profileImageSpec: ASLayoutSpec {
-        let cornerSpec = ASCornerLayoutSpec(child: profileImageNode, corner: addButton, location: .bottomRight)
+        let buttonRatioSpec = ASRatioLayoutSpec(ratio: 0.5, child: addButton)
+        let cornerSpec = ASCornerLayoutSpec(child: profileImageNode, corner: buttonRatioSpec, location: .bottomRight)
         // Slightly shift center of badge inside of avatar.
         cornerSpec.offset = CGPoint(x: -12, y: -12)
-        let inset =  ASInsetLayoutSpec(insets: .init(top: 10, left: 5, bottom: 0, right: 26), child: cornerSpec)
-        return inset
+        let inset =  ASInsetLayoutSpec(insets: .init(top: 10, left: 10, bottom: 0, right: 26), child: cornerSpec)
+        let ratioSpec = ASRatioLayoutSpec(ratio: 0.5, child: inset)
+        return ratioSpec
     }
     
     private func setupNodes() {
@@ -42,10 +45,10 @@ class ProfileHeaderAndStatsNode: BaseNode {
         profileImageNode.cornerRadius = 85/2
         
         addButton.backgroundColor = UIColor.colourStringWitHex(hexColour: "00008b", withAlpha: 1)
-        addButton.setTitle("+", with: UIFont.boldSystemFont(ofSize: 15), with: .white, for: .normal)
+        addButton.setTitle("+", with: UIFont.boldSystemFont(ofSize: 17), with: .white, for: .normal)
         addButton.borderColor = UIColor.white.cgColor
         addButton.borderWidth = 3
-        addButton.style.preferredSize = CGSize(width: 25, height: 25)
+        addButton.style.preferredSize = CGSize(width: 27, height: 27)
         addButton.cornerRadius = 25/2
         
     }
